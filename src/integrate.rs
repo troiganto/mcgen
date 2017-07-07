f623c5145ca48738f4026f28398af6b734b08ad1
Nico Madysa <nico.madysa@tu-dresden.de>
2017-07-08 00:35:09 +0200

    Add Integrate iterator in preparation for incremental integration.

--- src/integrate.rs
+++ src/integrate.rs
@@ -9,14 +9,48 @@ use super::Statistics;
 use super::Sample;
 
 
-pub fn integrate<F, X>(mut f: F, range: Range<X>, sample_size: usize) -> Statistics<X>
+pub struct Integrate<F, X>
 where
     F: FnMut(X) -> X,
     X: Float + SampleRange,
 {
-    let width = range.end - range.start;
-    Sample::new(distributions::Range::new(range.start, range.end))
-        .take(sample_size)
-        .map(|x| f(x) * width)
-        .collect()
+    func: F,
+    width: X,
+    sample: Sample<X, distributions::Range<X>>,
+}
+
+impl<F, X> Integrate<F, X>
+where
+    F: FnMut(X) -> X,
+    X: Float + SampleRange,
+{
+    pub fn new(f: F, range: Range<X>) -> Self {
+        let dist = distributions::Range::new(range.start, range.end);
+        Integrate {
+            func: f,
+            width: range.end - range.start,
+            sample: Sample::new(dist),
+        }
+    }
+}
+
+impl<F, X> Iterator for Integrate<F, X>
+where
+    F: FnMut(X) -> X,
+    X: Float + SampleRange,
+{
+    type Item = X;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.sample.next().map(|x| (self.func)(x) * self.width)
+    }
+}
+
+
+pub fn integrate<F, X>(f: F, range: Range<X>, sample_size: usize) -> Statistics<X>
+where
+    F: FnMut(X) -> X,
+    X: Float + SampleRange,
+{
+    Integrate::new(f, range).take(sample_size).collect()
 }
