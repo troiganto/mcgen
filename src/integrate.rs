fd1c9bfddbf6bb1831ff400b309abc3efc3261f7
Nico Madysa <nico.madysa@tu-dresden.de>
2017-07-14 01:29:48 +0200

    Extend Integrate to allow more functions.

--- src/integrate.rs
+++ src/integrate.rs
@@ -1,7 +1,7 @@
-use std::ops::Range;
+use std::ops;
+use std::cmp::PartialOrd;
 
-use num::Float;
-
+use num;
 use rand::Rng;
 use rand::distributions::range::SampleRange;
 use rand::distributions::{self, Sample, IndependentSample};
@@ -14,22 +14,24 @@ use super::{IntoSampleIter, Statistics};
 /// This struct is exposed to allow continuous inspection of
 /// the integration result and uncertainty. To just get the result
 /// immediately, use the function `integrate`.
-pub struct Integrate<F, X>
+pub struct Integrate<F, X, Y = X>
 where
-    F: FnMut(X) -> X,
-    X: Float + SampleRange,
+    F: FnMut(X) -> Y,
+    X: Copy + SampleRange + PartialOrd + ops::Sub<Output = X>,
+    Y: ops::Mul<X>,
 {
-    func: F,
-    width: X,
     x_sample: distributions::Range<X>,
+    width: X,
+    func: F,
 }
 
-impl<F, X> Integrate<F, X>
+impl<F, X, Y> Integrate<F, X, Y>
 where
-    F: FnMut(X) -> X,
-    X: Float + SampleRange,
+    F: FnMut(X) -> Y,
+    X: Copy + SampleRange + PartialOrd + ops::Sub<Output = X>,
+    Y: ops::Mul<X>,
 {
-    pub fn new(f: F, range: Range<X>) -> Self {
+    pub fn new(f: F, range: ops::Range<X>) -> Self {
         Integrate {
             func: f,
             width: range.end - range.start,
@@ -38,23 +40,25 @@ where
     }
 }
 
-impl<F, X> Sample<X> for Integrate<F, X>
+impl<F, X, Y> Sample<<Y as ops::Mul<X>>::Output> for Integrate<F, X, Y>
 where
-    F: FnMut(X) -> X,
-    X: Float + SampleRange,
+    F: FnMut(X) -> Y,
+    X: Copy + SampleRange + PartialOrd + ops::Sub<Output = X>,
+    Y: ops::Mul<X>,
 {
-    fn sample<R: Rng>(&mut self, rng: &mut R) -> X {
+    fn sample<R: Rng>(&mut self, rng: &mut R) -> <Y as ops::Mul<X>>::Output {
         let x = self.x_sample.sample(rng);
         (self.func)(x) * self.width
     }
 }
 
-impl<F, X> IndependentSample<X> for Integrate<F, X>
+impl<F, X, Y> IndependentSample<<Y as ops::Mul<X>>::Output> for Integrate<F, X, Y>
 where
-    F: Fn(X) -> X,
-    X: Float + SampleRange,
+    F: Fn(X) -> Y,
+    X: Copy + SampleRange + PartialOrd + ops::Sub<Output = X>,
+    Y: ops::Mul<X>,
 {
-    fn ind_sample<R: Rng>(&self, rng: &mut R) -> X {
+    fn ind_sample<R: Rng>(&self, rng: &mut R) -> <Y as ops::Mul<X>>::Output {
         let x = self.x_sample.ind_sample(rng);
         (self.func)(x) * self.width
     }
@@ -65,10 +69,17 @@ where
 ///
 /// This function integrates via Mone-Carlo methods. `sample_size` is
 /// a measure of the integration precision.
-pub fn integrate<F, X, R>(f: F, range: Range<X>, sample_size: usize, rng: &mut R) -> Statistics<X>
+pub fn integrate<F, X, Y, R>(
+    f: F,
+    range: ops::Range<X>,
+    sample_size: usize,
+    rng: &mut R,
+) -> Statistics<<Y as ops::Mul<X>>::Output>
 where
-    F: FnMut(X) -> X,
-    X: Float + SampleRange,
+    F: FnMut(X) -> Y,
+    X: Copy + SampleRange + PartialOrd + ops::Sub<Output = X>,
+    Y: ops::Mul<X>,
+    Y::Output: num::Float,
     R: Rng,
 {
     Integrate::new(f, range)
